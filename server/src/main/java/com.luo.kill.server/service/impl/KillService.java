733b758874ffd5dff2bb93bcca6dd6b7763acaf4
Administrator <2084267015@qq.com>
2019-10-15 21:40:55 +0800

    base

--- server/src/main/java/com.luo.kill.server/service/impl/KillService.java
+++ server/src/main/java/com.luo.kill.server/service/impl/KillService.java
@@ -2,6 +2,7 @@ package com.luo.kill.server.service.impl;
 
 import com.luo.kill.model.entity.ItemKill;
 import com.luo.kill.model.entity.ItemKillSuccess;
+import com.luo.kill.model.mapper.ItemKillMapper;
 import com.luo.kill.model.mapper.ItemKillSuccessMapper;
 import com.luo.kill.server.utils.RandomUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,22 +13,39 @@ public class KillService {
 
     @Autowired
     private ItemKillSuccessMapper itemKillSuccessMapper;
+    @Autowired
+    private ItemKillMapper itemKillMapper;
 
-    public void kill(Integer killId, Integer userId) {
+    public boolean kill(Integer killId, Integer userId) {
+        boolean result = false;
+        if (itemKillSuccessMapper.countByKillUserId(killId, userId) <= 0) {
+            ItemKill itemKill = itemKillMapper.selectById(killId);
+            if (itemKill != null && itemKill.getCanKill() == 1) {
+                int res = itemKillMapper.updateKillItem(killId);
+                if (res > 0) {
+                    commonRecordKillSuccessInfo(itemKill, userId);
+                    result = true;
+                }
+            }
+
+        }
+        return result;
+    }
+
+    private boolean commonRecordKillSuccessInfo(ItemKill itemKill, Integer userId) {
         ItemKillSuccess entity = new ItemKillSuccess();
         //snow flake
         entity.setCode(RandomUtil.generateOrderCode());
-        //entity.setItemId(itemKill.getItemId());
-        entity.setKillId(killId);
+        entity.setItemId(itemKill.getItemId());
+        entity.setKillId(itemKill.getItemId());
         entity.setUserId(userId.toString());
-
 
         int res = itemKillSuccessMapper.insertSelective(entity);
 
         if (res > 0) {
 
         }
-
+        return true;
 
     }
 
