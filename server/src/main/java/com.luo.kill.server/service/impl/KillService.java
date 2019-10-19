ac99a6439cd18133e5c4195df37353607dee47c3
Administrator <2084267015@qq.com>
2019-10-19 19:37:14 +0800

    base

--- server/src/main/java/com.luo.kill.server/service/impl/KillService.java
+++ server/src/main/java/com.luo.kill.server/service/impl/KillService.java
@@ -6,10 +6,14 @@ import com.luo.kill.model.mapper.ItemKillMapper;
 import com.luo.kill.model.mapper.ItemKillSuccessMapper;
 import com.luo.kill.server.service.RabbitSenderService;
 import com.luo.kill.server.utils.RandomUtil;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class KillService {
@@ -22,6 +26,9 @@ public class KillService {
     private RabbitSenderService rabbitSenderService;
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedissonClient redissonClient;
 
 
     //库存超卖
@@ -56,16 +63,19 @@ public class KillService {
         return result;
     }
 
-    public boolean kill2(Integer killId, Integer userId) throws Exception {
+    public boolean killV2(Integer killId, Integer userId) throws Exception {
         boolean result = false;
         if (itemKillSuccessMapper.countByKillUserId(killId, userId) <= 0) {
+
             //redis 原子操作
             ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
             final String key = new StringBuffer().append(killId).append("-")
                     .append(userId).toString();
             final String value = RandomUtil.generateOrderCode();
             Boolean cacheResult = ops.setIfAbsent(key, value);
-            if (cacheResult) {
+            //redis 挂掉了，会出现锁死
+            if (cacheResult) {//会出现锁死
+                stringRedisTemplate.expire(key, 30, TimeUnit.MILLISECONDS);
                 try {
                     ItemKill itemKill = itemKillMapper.selectByIdV2(killId);
                     if (itemKill != null && itemKill.getCanKill() == 1) {
@@ -94,6 +104,46 @@ public class KillService {
         }
         return result;
     }
+
+    public boolean killV3(Integer killId, Integer userId) {
+        boolean result = false;
+        final String lockKey = new StringBuffer().append(killId).append("-")
+                .append(userId).toString();
+        RLock lock = redissonClient.getLock(lockKey);
+
+        try {
+
+            boolean cacheRes = lock.tryLock(100, 10, TimeUnit.MILLISECONDS);
+            if (cacheRes) {
+                //核心业务
+                if (itemKillSuccessMapper.countByKillUserId(killId, userId) <= 0) {
+                    ItemKill itemKill = itemKillMapper.selectById(killId);
+                    if (itemKill != null && itemKill.getCanKill() == 1) {
+                        int res = itemKillMapper.updateKillItem(killId);
+                        //同一个 user 抢购一次，缺点：高并发的时候，需要不断的访问mysql
+                        if (itemKillSuccessMapper.countByKillUserId(killId, userId) <= 0) {
+                            if (res > 0) {
+                                //先读后写：thread-safe 问题
+                                commonRecordKillSuccessInfo(itemKill, userId);
+                                result = true;
+                            }
+                        }
+                    }
+
+                }
+            }
+
+
+        } catch (Exception e) {
+
+        } finally {
+            lock.unlock();
+            // lock.forceUnlock();//force 强制解锁
+        }
+
+        return result;
+    }
+
     private boolean commonRecordKillSuccessInfo(ItemKill itemKill, Integer userId) {
         ItemKillSuccess entity = new ItemKillSuccess();
         //snow flake
